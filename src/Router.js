f5eb31345264593d645120b91639f2cc554792fb
Juan Manuel Lopez <juanlg@me.com>
2019-07-04 17:13:28 -0500

    Created Add Button in EmployeeList Component

--- src/Router.js
+++ src/Router.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import console = require('console');
 
 const RouterComponent = () => {
   return (
     <Router>
-        <Scene key="root">
-            <Scene key="login" component={LoginForm} title="Please Login" initial />
-            <Scene key="employeeList" component={EmployeeList} title="Employees" />
+        <Scene key="root" hideNavBar>
+            <Scene key="auth">
+                <Scene key="login" component={LoginForm} title="Please Login" initial />
+            </Scene>
+            <Scene key="main">
+                <Scene
+                    rightTitle="+"
+                    onRight = { () => { console.log("Right event!!!!!")}}
+                    key="employeeList"
+                    component={EmployeeList}
+                    title="Employees" />
+                </Scene>
         </Scene>
     </Router>
   );
